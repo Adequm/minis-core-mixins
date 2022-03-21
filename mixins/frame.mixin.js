9383c7728f7c01efceb47afa8027291ca43570ec
Adequm <adequm@bk.ru>
2022-03-22 01:11:52 +0300

    update frame.mixin

--- mixins/frame.mixin.js
+++ mixins/frame.mixin.js
@@ -3,10 +3,62 @@ import _ from 'lodash';
 export default {
   data: () => ({
     appIndex: 0,
+    framePageIndex: 0,
     isFullscreenInFrame: false,
+    links: ['self'],
   }),
 
+  computed: {
+    domen() {
+      return process.env.NODE_ENV === 'development' && location.pathname.startsWith('/minis/')
+        ? `${ location.origin }/minis#`
+        : 'https://adequm.github.io/minis-';
+    },
+  },
+
+  methods: {
+    getSettingsButton(index = 0, amount = 0) {
+      return new Promise(async resolve => {
+        try {
+          const body = window.frames[index].document.body;
+          const settingsButton = body.querySelector(`#settingsButton`);
+          if(!settingsButton) throw settingsButton;
+          resolve(settingsButton);
+        } catch(err) {
+          if(amount >= 50 || !this.isFullscreen) return resolve(null);
+          await new Promise(resolve => setTimeout(resolve, 200));
+          resolve(this.getSettingsButton(index, ++amount));
+        }
+      })
+    },
+    initFramesSettingsWatcher() {
+      _.each(this.links, (link, index) => {
+        this.initFrameSettingsWatcher(index);
+      })
+    },
+    async initFrameSettingsWatcher(index) {
+      if(!this.isFullscreen) return;
+      const settingsButton = await this.getSettingsButton(index);
+      _.invoke(settingsButton, 'setAttribute', 'loading', 'done');
+      _.invoke(settingsButton, 'addEventListener', 'click', () => {
+        this.openedModalName = 'settings';
+        this.isClosedSettings = false;
+      });
+    },
+
+    clickToArrow(side) {
+      const length = this.links.length;
+      if(side == 'left') {
+        this.framePageIndex = ((this.framePageIndex + length) - 1) % length;
+      } else {
+        this.framePageIndex = (this.framePageIndex + 1) % length;
+      }
+      this.initFramesSettingsWatcher();
+    },
+  },
+
   beforeMount() {
+    this.initFramesSettingsWatcher();
     const locationsGet = _.chain(location.href)
       .split('?')
       .last()
